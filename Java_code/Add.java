a9019881d104ee044bc83ff08cfcd70da51280c6
nehashedge30 <nehashedge3012@gmail.com>
2025-01-04 09:41:56 +0530

    Project changing

---
+++ Java_code/Add.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Add{
+    public static void main(String[] args)
+    {
+
+
+
+        
+        // System.out.println("Addition of numbers");
+        // Scanner sc = new Scanner(System.in);
+
+        
+        // int sum=0;
+
+        // for(int i=0;i<5;i++)
+        // {
+        //     System.out.println("Enter numbers: ");
+        //     int n =sc.nextInt();
+        //     sum=sum+n;
+        // }
+        // System.out.println("sum is: "+sum);
+    }
+}
